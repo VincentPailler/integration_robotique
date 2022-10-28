3c31cb4f0ec5b60d6a34a17653c9d79a48fc6d04
vincentpailler <20100pailler@gmail.com>
2022-10-28 23:26:17 +0200

    ajout des version finales des programmes

--- README.md
+++ README.md
@@ -7,10 +7,11 @@ renommer le dossier integration robotique en catkin_ws
 
 Ouvrir un second terminal
 
-Faire les commandes suivantes dans les 2 terminaux
+Faire les commandes suivantes dans les 2 terminaux :
 . /opt/ros/noetic/setup.bash
-catkin build
 . devel/setup.bash
+
+catkin build
 
 Dans un terminal lancez Rviz avec la commande suivante:
 roslaunch hc10_move_config demo.launch
@@ -23,3 +24,7 @@ python3 src/trajectoire.py
 Pour ajouter gazebo, dans un terminal lancez la commande suivante:
 roslaunch hc10_move_config demo_gazebo.launch
 
+Ajouter une boîte en x=0, y=1, z=0.2 avec une épaisseur de 0.2
+
+Une fois cela fait lancer le programme python. ATTENTION nos machines n'ont pas pu supporter le lancement de ce programme.
+python3 src/trajectoire_finale.py
--- src/trajectoire_finale.py
+++ src/trajectoire_finale.py
@@ -8,11 +8,31 @@ import rospy
 import moveit_commander
 import moveit_msgs.msg
 import geometry_msgs.msg
+import numpy as np
 
 from math import pi, tau, dist, fabs, cos
 
 from std_msgs.msg import String
 from moveit_commander.conversions import pose_to_list
+
+def get_quaternion_from_euler(roll, pitch, yaw):
+  """
+  Convert an Euler angle to a quaternion.
+   
+  Input
+    :param roll: The roll (rotation around x-axis) angle in radians.
+    :param pitch: The pitch (rotation around y-axis) angle in radians.
+    :param yaw: The yaw (rotation around z-axis) angle in radians.
+ 
+  Output
+    :return qx, qy, qz, qw: The orientation in quaternion [x,y,z,w] format
+  """
+  qx = np.sin(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) - np.cos(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
+  qy = np.cos(roll/2) * np.sin(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.cos(pitch/2) * np.sin(yaw/2)
+  qz = np.cos(roll/2) * np.cos(pitch/2) * np.sin(yaw/2) - np.sin(roll/2) * np.sin(pitch/2) * np.cos(yaw/2)
+  qw = np.cos(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
+ 
+  return [qx, qy, qz, qw]
 
 # connecte le robot à rviz
 moveit_commander.roscpp_initialize(sys.argv)
@@ -31,34 +51,54 @@ rospy.sleep(2)
 # coordonnée de la boite : x=0 ; y = 1 ; z = 0.2
 # taille cube de 0.2 de coté.
 
+qx,qy,qz,qw = get_quaternion_from_euler(0,pi/2,0)
+
 # définition des points à atteindre 
 pose_goal = []
 pose_goal0 = geometry_msgs.msg.Pose()
-pose_goal0.orientation.w = 1.0
+pose_goal0.orientation.x = qx
+pose_goal0.orientation.y = qy
+pose_goal0.orientation.z = qz
+pose_goal0.orientation.w = qw
 pose_goal0.position.x = -0.3
 pose_goal0.position.y = 1
 pose_goal0.position.z = 0.2
 pose_goal.append(pose_goal0)
 
+qx,qy,qz,qw = get_quaternion_from_euler(0,-pi/2,0)
+
 pose_goal1 = geometry_msgs.msg.Pose()
-pose_goal1.orientation.w = 1.0
+pose_goal1.orientation.x = qx
+pose_goal1.orientation.y = qy
+pose_goal1.orientation.z = qz
+pose_goal1.orientation.w = qw
 pose_goal1.position.x = 0.3
 pose_goal1.position.y = 1
 pose_goal1.position.z = 0.2
 pose_goal.append(pose_goal1)
 
+qx,qy,qz,qw = get_quaternion_from_euler(-pi/2,0,0)
+
 pose_goal2 = geometry_msgs.msg.Pose()
-pose_goal2.orientation.w = 1.0
+pose_goal2.orientation.x = qx
+pose_goal2.orientation.y = qy
+pose_goal2.orientation.z = qz
+pose_goal2.orientation.w = qw
 pose_goal2.position.x = 0
 pose_goal2.position.y = 0.3
-pose_goal2.position.z = 1
+pose_goal2.position.z = 0.2
 pose_goal.append(pose_goal2)
 
+qx,qy,qz,qw = get_quaternion_from_euler(pi/2,0,0)
+
 pose_goal3 = geometry_msgs.msg.Pose()
-pose_goal3.orientation.w = 1.0
+pose_goal3.orientation.x = qx
+pose_goal3.orientation.y = qy
+pose_goal3.orientation.z = qz
+pose_goal3.orientation.w = qw
 pose_goal3.position.x = 0
-pose_goal3.position.y = 0.6
-pose_goal3.position.z = 1
+pose_goal3.position.y = 1.4
+pose_goal3.position.z = 0.2
 pose_goal.append(pose_goal3)
 
 # génération de la trajectoire pour atteindre chaque point
